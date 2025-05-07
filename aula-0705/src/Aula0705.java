3ed8d2b53d2bf08079c0a2d827cf2429e41d9755
mariaferabelo <mariafernandamrabelo@gmail.com>
2025-05-07 19:09:31 -0300

    códigos aula do dia 07/05

---
+++ aula-0705/src/Aula0705.java
@@ -0,0 +1,7 @@
+public class Aula0705 {
+
+    public static void main(String[] args) {
+        Tela t = new Tela();
+        t.configurarJanela();
+    }
+}
---
+++ aula-0705/src/Tela.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.*;
+
+public class Tela extends JFrame {
+    private JPanel painel;
+    private JButton[] jbBotoes;
+    
+    
+    public Tela(){
+        painel = new JPanel();
+        jbBotoes = new JButton[5];
+        for (int i=0; i<5; i++){
+            jbBotoes[i] = new JButton("Botao "+(i+1));
+        }
+
+    }
+
+    public void configurarJanela(){
+        setVisible(true);
+        setTitle("Aula 30/04");
+        setSize(600,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocation(600, 500);
+        add(painel);
+        configurarPainel();
+
+    }
+
+    public void configurarPainel(){
+        painel.setBackground(Color.PINK);
+        painel.setLayout(new FlowLayout(FlowLayout.RIGHT, 20, 20));
+        for (int i=0; i<5; i++){
+            painel.add (jbBotoes[i]);
+        }
+        
+    }
+
+
+
+}
